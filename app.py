db2dc826ab3ddf5af1ccc4dacc848348ae27a962
Carlos Castaño <carlose.castano@udea.edu.co>
2025-10-08 21:38:59 -0500

    Archivo translations.py. Contiene un diccionario para traducir el sitio web

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import os
 import pickle
 import streamlit as st
 from streamlit_option_menu import option_menu
+from translations import translations
 
 # Set page configuration
 st.set_page_config(page_title="Health Assistant",
@@ -13,15 +14,19 @@ st.set_page_config(page_title="Health Assistant",
 working_dir = os.path.dirname(os.path.abspath(__file__))
 
 # loading the saved models
-
 diabetes_model = pickle.load(open(f'{working_dir}/saved_models/diabetes_model.sav', 'rb'))
-
 heart_disease_model = pickle.load(open(f'{working_dir}/saved_models/heart_disease_model.sav', 'rb'))
-
 parkinsons_model = pickle.load(open(f'{working_dir}/saved_models/parkinsons_model.sav', 'rb'))
 
 # sidebar for navigation
 with st.sidebar:
+    # --- Selector de idioma ---
+    st.markdown("<h2 style='text-align: center;'>🌐</h2>", unsafe_allow_html=True)
+    language = st.toggle("🇨🇴 Español / 🇬🇧 English", value=True)
+    selected_language = "es" if language else "en"
+    t = translations[selected_language]
+    st.write("")
+
     selected = option_menu('Multiple Disease Prediction System',
 
                            ['Diabetes Prediction',
@@ -36,35 +41,43 @@ with st.sidebar:
 if selected == 'Diabetes Prediction':
 
     # page title
-    st.title('Diabetes Prediction using ML')
+    #st.title('Diabetes Prediction using ML')
+    st.title(t["title"])
 
     # getting the input data from the user
     col1, col2, col3 = st.columns(3)
 
     with col1:
-        Pregnancies = st.text_input('Number of Pregnancies')
+        #Pregnancies = st.text_input('Number of Pregnancies')
+        Pregnancies = st.text_input(t["pregnancies"])
 
     with col2:
-        Glucose = st.text_input('Glucose Level')
+        #Glucose = st.text_input('Glucose Level')
+        Glucose = st.text_input(t["glucose_level"])
 
     with col3:
-        BloodPressure = st.text_input('Blood Pressure value')
+        #BloodPressure = st.text_input('Blood Pressure value')
+        BloodPressure = st.text_input(t["blood_pressure"])
 
     with col1:
-        SkinThickness = st.text_input('Skin Thickness value')
+        #SkinThickness = st.text_input('Skin Thickness value')
+        SkinThickness = st.text_input(t["skin_thickness"])
 
     with col2:
-        Insulin = st.text_input('Insulin Level')
+        #Insulin = st.text_input('Insulin Level')
+        Insulin = st.text_input(t["insulin_level"])
 
     with col3:
-        BMI = st.text_input('BMI value')
+        #BMI = st.text_input('BMI value')
+        BMI = st.text_input(t["bmi"])
 
     with col1:
-        DiabetesPedigreeFunction = st.text_input('Diabetes Pedigree Function value')
+        #DiabetesPedigreeFunction = st.text_input('Diabetes Pedigree Function value')
+        DiabetesPedigreeFunction = st.text_input(t["diabetes_pedigree_function"])
 
     with col2:
-        Age = st.text_input('Age of the Person')
-
+        #Age = st.text_input('Age of the Person')
+        Age = st.text_input(t["age_of_the_person"])
 
     # code for Prediction
     diab_diagnosis = ''
---
+++ translations.py
@@ -0,0 +1,30 @@
+translations = {
+    "es": {
+        "title": "Predicción de Diabetes con ML",
+        "pregnancies": "Número de embarazos",
+        "glucose_level": "Nivel de glucosa",
+        "blood_pressure": "Presión arterial",
+        "skin_thickness": "Espesor de la piel",
+        "insulin_level": "Nivel de insulina",
+        "bmi": "Índice de masa corporal",
+        "diabetes_pedigree_function": "Función de herencia de la diabetes",
+        "age_of_the_person": "Edad de la persona",
+        "button": "Resultado del test de diabetes",
+        "diagnosis_positive": "La persona puede ser diabética, consulte a su médico.",
+        "diagnosis_negative": "La persona no es diabética."
+    },
+    "en": {
+        "title": "Diabetes Prediction using ML",
+        "pregnancies": "Number of Pregnancies",
+        "glucose_level": "Glucose Level",
+        "blood_pressure": "Blood Pressure",
+        "skin_thickness": "Skin Thickness",
+        "insulin_level": "Insulin Level",
+        "bmi": "Body Mass Index",
+        "diabetes_pedigree_function": "Diabetes Pedigree Function",
+        "age_of_the_person": "Age of the Person",
+        "button": "Diabetes Test Result",
+        "diagnosis_positive": "The person may be diabetic, consult your doctor.",
+        "diagnosis_negative": "The person is not diabetic."
+    }
+}
